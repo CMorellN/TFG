dbcebd473c6e2688247ce0fe63cbfc7d69760c7c
CMorellN <carlamorelln@gmail.com>
2026-04-17 18:25:04 +0200

    new graphs

--- do_pca.py
+++ do_pca.py
@@ -14,10 +14,18 @@ def _():
     import mpl_toolkits.mplot3d # Para visualizar 3D
     import plotly.express as px
     from scatter3d import Scatter3dWidget, Category, LabelListErrorResponse
-    import pandas
-    import numpy
+    import pandas as pd
+    import numpy as np
 
-    return Category, Scatter3dWidget, mo, pandas, plt, pynei
+    return Category, Scatter3dWidget, mo, np, plt, pynei
+
+
+@app.cell
+def _():
+    # ================================================================================================
+    # ===================== PCA / PCoA   F U N C T I O N S   F R O M   P Y N E I =====================
+    # ================================================================================================
+    return
 
 
 @app.cell(hide_code=True)
@@ -132,6 +140,14 @@ def _(mo):
 
 
 @app.cell
+def _():
+    # ================================================================================================
+    # ================================ F I L E  P R E P A R A T I O N ================================
+    # ================================================================================================
+    return
+
+
+@app.cell
 def _(mo):
     button_file = mo.ui.file(multiple=True, kind='button' ,label='Select file')
     mo.hstack([mo.md("Input VCF: "), button_file], justify="start")
@@ -150,8 +166,10 @@ def _(button_file, mo):
 
 @app.cell
 def _(button_file, pynei):
+    # Preparing the file to PCA:
+
     if button_file.value:
-        # Preparar para PCA
+
         import tempfile
 
         if button_file.value is not None:
@@ -163,6 +181,14 @@ def _(button_file, pynei):
 
             data
     return (data,)
+
+
+@app.cell
+def _():
+    # ===============================================================================================
+    # ================================== PCA / PCoA  O P T I O N S ==================================
+    # ===============================================================================================
+    return
 
 
 @app.cell
@@ -182,28 +208,16 @@ def _(mo):
 
 
 @app.cell
-def _(mo):
-    checkbox = mo.ui.checkbox(label="Show and edit parameters:")# Create a checkbox to change PCA or PCoA parameters
-    checkbox
-    return (checkbox,)
-
-
-@app.cell
-def _(checkbox, mo, slider0, slider1, slider2, slider3):
-    if checkbox.value:
-        _parameters = mo.vstack([slider0, slider1, slider2, slider3])
-    _parameters
+def _(mo, slider0, slider1, slider2, slider3):
+    mo.accordion({"**Show and edit parameters:** ": mo.vstack([slider0, slider1, slider2, slider3])})
     return
 
 
 @app.cell
-def _(mo):
-    mo.md("""
-    /// details | Default parameters
-        type: info
-
-        texto de relleno
-    """)
+def _():
+    # ===============================================================================================
+    # ================================ PCA / PCoA  E X E C U T I O N ================================
+    # ===============================================================================================
     return
 
 
@@ -255,7 +269,67 @@ def _(
 
 
 @app.cell
+def _():
+    # ===============================================================================================
+    # ============================= P R E P A R I N G   G R A P H I C S =============================
+    # ===============================================================================================
+    return
+
+
+@app.cell
+def _(mo, results):
+    # 2D PROJECTIONS - buttons:
+    pc_index = results["projections"].axes[1] # Take the projections indexes 'PC00', 'PC01'...
+
+    dropdown_x = mo.ui.dropdown(options=list(pc_index), value=pc_index[0], label='Horizontal axis: ')
+    dropdown_y = mo.ui.dropdown(options=list(pc_index), value=pc_index[1], label='Vertical axis: ')
+    return dropdown_x, dropdown_y, pc_index
+
+
+@app.cell
+def _(dropdown_x, dropdown_y, plt, results):
+    # 2D PROJECTIONS with scatter:
+
+     # (Note: dorpdown_x.value is the same that dropdown_x.selected_key)
+
+    widget_2d = plt.scatter(results["projections"][dropdown_x.selected_key], results["projections"][dropdown_y.selected_key])
+    plt.title('2D Projections')
+    plt.xlabel(dropdown_x.selected_key)
+    plt.ylabel(dropdown_y.selected_key)
+    plt.close()
+    return (widget_2d,)
+
+
+@app.cell
+def _(mo, pc_index):
+    # 3D PROJECTIONS - buttons:
+    dropdown_x_3d = mo.ui.dropdown(options=list(pc_index), value=pc_index[0], label='Horizontal axis: ')
+    dropdown_y_3d = mo.ui.dropdown(options=list(pc_index), value=pc_index[1], label='Vertical axis: ')
+    dropdown_z_3d = mo.ui.dropdown(options=list(pc_index), value=pc_index[2], label='Depth axis: ')
+    return dropdown_x_3d, dropdown_y_3d, dropdown_z_3d
+
+
+@app.cell
+def _(dropdown_x_3d, dropdown_y_3d, dropdown_z_3d, plt, results):
+    # 3d PROJECTIONS with scatter:
+
+    figure_3d = plt.figure()
+    ax = figure_3d.add_subplot(111, projection='3d')
+
+    scatter = ax.scatter(results["projections"][dropdown_x_3d.value], results["projections"][dropdown_y_3d.value], results["projections"][dropdown_z_3d.value])
+
+    ax.set_title('3D Projections')
+    ax.set_xlabel(dropdown_x_3d.value)
+    ax.set_ylabel(dropdown_y_3d.value)
+    ax.set_zlabel(dropdown_z_3d.value)
+    plt.close()
+    return (figure_3d,)
+
+
+@app.cell
 def _(Category, Scatter3dWidget, pandas, results):
+    # 3D PROJECTIONS with Scatter3dWidget from Pynei:
+
     # 1. Unificar datos en una variable
     xyz = results["projections"].iloc[:,:3]
 
@@ -274,32 +348,219 @@ def _(Category, Scatter3dWidget, pandas, results):
 
 
 @app.cell
-def _(widget_3d):
-    widget_3d
+def _(plt, results):
+    # EXPLAINED VARIANCE:
+
+    fig, axis = plt.subplots()
+
+    cum_var = results["explained_variance (%)"].cumsum() # The cumulative variance
+
+    cum_var.plot(kind="bar", ax=axis, color="orange", label="Cumulative variance") # cumulative variance - BARS
+    # cum_var.plot(ax=axis, color="red", marker="o", linestyle="-", label="Cumulative variance") # cumulative variance - LINES
+
+    results["explained_variance (%)"].plot(kind="bar", ax=axis, color="blue", label="Individual variance") # Explained variance (not cumulative)
+
+    axis.set_title('Cumulative variance')
+    axis.set_xlabel('Principal components')
+    axis.set_ylabel('Explained variance (%)')
+    axis.legend()
+
+    axis.set_yticks(range(0, 101, 10))
+    axis.grid(axis='y')
+
+    # fig
+    return (fig,)
+
+
+@app.cell
+def _():
+    # ===============================================================================================
+    # ================================= V I S U A L I Z A T I O N S =================================
+    # ===============================================================================================
+    return
+
+
+@app.cell
+def _(np, plt, results):
+    figSento, axSento = plt.subplots()
+
+    ejex = results["princomps"].iloc[0,:]
+    ejey = results["princomps"].iloc[1,:]
+
+    plt.scatter(ejex, ejey)
+
+    # Make vectors (or arrows) without loops:
+    plt.quiver(
+        np.zeros_like(ejex),  # origen X (todos 0)
+        np.zeros_like(ejey),  # origen Y (todos 0)
+        ejex,
+        ejey,
+        angles='xy',
+        scale_units='xy',
+        scale=1,
+        width=0.005,
+        color='red',
+        alpha=0.5
+    )
+
+    for varkk in results["princomps"].columns:
+        ldx = results["princomps"].loc["PC00", varkk]
+        ldy = results["princomps"].loc["PC01", varkk]
+
+        # axSento.arrow(
+        #     0, 0,
+        #     ldx,
+        #     ldy,
+        #     color="red",
+        #     alpha=0.6
+        # )
+
+        axSento.text(
+            ldx,
+            ldy,
+            str(varkk),
+            color="red",
+            fontsize=8
+        )
+
+    plt.title("Rotación de ejes PCA")
+    plt.xlabel('PC00')
+    plt.ylabel('PC01')
+
+    plt.axhline(0, color='grey', linewidth=0.5)
+    plt.axvline(0, color="grey", linewidth=0.5)
+    return (figSento,)
+
+
+@app.cell
+def _(plt, results):
+    figkk, axkk = plt.subplots(figsize=(8, 6))
+
+    # 👉 puntos (projections)
+    x = results["projections"]["PC00"]
+    y = results["projections"]["PC01"]
+
+    # axkk.scatter(x, y, alpha=0.5)
+
+    # 👉 loadings desde TU objeto
+    loadings = results["princomps"]
+
+
+    # 👉 top variables (más importantes en PC00)
+    # pc1 = loadings.loc["PC00"]
+
+    # top_vars = pc1.abs().sort_values(ascending=False).head(10).index
+
+    # print(loadings)
+    # escala para visualizar
+    scale = 5
+
+    for var in loadings.columns:
+        lx = loadings.loc["PC00", var]
+        ly = loadings.loc["PC01", var]
+
+        axkk.arrow(
+            0, 0,
+            lx * scale,
+            ly * scale,
+            color="red",
+            alpha=0.6
+        )
+
+        axkk.text(
+            lx * scale,
+            ly * scale,
+            str(var),
+            color="red",
+            fontsize=8
+        )
+    
+    #     print(var, " ===", lx)
+    # print(results["princomps"].iloc[0])
+
+    axkk.set_xlabel("x")
+    axkk.set_ylabel("y")
+    axkk.set_title("Rotación de ejes (PCA)")
+
+    plt.axhline(0, color="grey", linewidth=0.5)
+    plt.axvline(0, color="grey", linewidth=0.5)
+
+    plt.close(figkk)
+
+    figkk
     return
 
 
 @app.cell
 def _(plt, results):
-    widget_2d = plt.scatter(results["projections"].iloc[:,0], results["projections"].iloc[:,1])
 
-    return (widget_2d,)
-
-
-@app.cell
-def _():
-    # figure = plt.figure()
-    # ax = figure.add_subplot([111], projection='3d')
-    # scatter = ax.scatter(results["projections"].iloc[:,0], results["projections"].iloc[:,1], results["projections"].iloc[:,2])
+    kk = plt.scatter(results["princomps"][0], results["princomps"][1])
+    plt.title('What is this?')
+    # plt.xlabel()
+    # plt.ylabel()
+    # plt.close()
     return
 
 
 @app.cell
-def _(mo, results, widget_2d, widget_3d):
+def _(results):
+    results["princomps"].loc["PC00", :]
+    return
+
+
+@app.cell
+def _(results):
+    results["projections"]
+    return
+
+
+@app.cell
+def _(
+    dropdown_x,
+    dropdown_x_3d,
+    dropdown_y,
+    dropdown_y_3d,
+    dropdown_z_3d,
+    fig,
+    figSento,
+    figure_3d,
+    mo,
+    results,
+    widget_2d,
+    widget_3d,
+):
+    # # VERSIÓN 1:
+    # tabs = mo.ui.tabs({
+    #     "Projections 2d scatter": mo.vstack([widget_2d, results["projections"]]),
+    #     "Projections 3d scatter": mo.vstack([figure_3d, results["projections"]]),
+    #     "Projections 3d tutor": mo.vstack([widget_3d, results["projections"]]),
+    #     "Explaines variance": mo.vstack([widget_2d, results["explained_variance (%)"]]),
+    #     # "Principal components": 
+    #     })
+    # tabs
+
+    # VERSIÓN 2:
     tabs = mo.ui.tabs({
-        "Projections 0d": widget_3d,
-        "Projections 2d": mo.vstack([widget_2d, results["projections"]]),
-        "Projections 3d": mo.vstack([widget_3d, results["projections"]])
+        "Projections 2d scatter": mo.ui.tabs({
+            'Graphs': mo.hstack([widget_2d, mo.vstack([dropdown_x, dropdown_y])]),
+            'Data': results["projections"]
+            }),
+
+        "Projections 3d scatter":  mo.ui.tabs({
+            'Graphs': mo.hstack([figure_3d, mo.vstack([dropdown_x_3d, dropdown_y_3d, dropdown_z_3d])]),
+            'Data': results["projections"]
+            }),
+
+        "Projections 3d tutor":  mo.ui.tabs({
+            'Graphs': widget_3d,
+            'Data': results["projections"]
+            }),
+
+        "Explaines variance":  mo.ui.tabs({
+            'Graphs': fig,
+            'Data': results["explained_variance (%)"]
+            }),
+        "Principal components": figSento, 
         })
     tabs
     return
